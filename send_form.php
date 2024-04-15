430e004831ac95e5dc2981e5fd8224b7b0ada0dd
Samuel Dillenburg <sam.studi@gmail.com>
2024-04-15 11:00:16 +0200

    send form logic
    added missing php file for handling incoming form data

---
+++ send_form.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $to = "sam.studi@gmail.com"; // HIER EMAIL ANGEBEN !
+    $subject = "Neue Kontaktanfrage von $name";
+    $body = "Name: $name\n\nE-Mail: $email\n\nNachricht: $message";
+    mail($to, $subject, $body);
+
+    
+    echo json_encode(["success" => true]);
+} else {
+    echo json_encode(["error" => "Formular konnte nicht gesendet werden"]);
+}
+?>
